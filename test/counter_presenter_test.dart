3eb77438c0bcab1a5e0f2f69073e8a6e249f016f
Dmytro Turskyi <dmytro@turskyi.com>
2024-08-07 18:48:40 -0400

    :recycle: Refactor counter presenter and increment counter services in tests
    Signed-off-by: Dmytro Turskyi <dmytro.turskyi@gmail.com>

--- test/counter_presenter_test.dart
+++ test/counter_presenter_test.dart
@@ -1,14 +1,16 @@
 import 'package:async/async.dart';
 import 'package:counter_with_onion_architecture/core/application_services/counter_presenter.dart';
 import 'package:counter_with_onion_architecture/core/domain/model/counter.dart';
+import 'package:counter_with_onion_architecture/core/domain/services/increment_counter.dart';
 import 'package:counter_with_onion_architecture/core/domain/services/increment_counter_fake_impl.dart';
+import 'package:counter_with_onion_architecture/infrastructure/counter_data_source.dart';
 import 'package:counter_with_onion_architecture/infrastructure/fake_counter_data_source.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   test('CounterPresenter should update the counter', () async {
-    final FakeCounterDataSource dataSource = FakeCounterDataSource();
-    final IncrementCounterFakeImpl incrementCounter = IncrementCounterFakeImpl(
+    final CounterDataSource dataSource = FakeCounterDataSource();
+    final IncrementCounter incrementCounter = IncrementCounterFakeImpl(
       dataSource,
     );
     final CounterPresenter presenter = CounterPresenter(incrementCounter);
